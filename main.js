6696fad2f6edc3b0119b6c3335c42e25a4540e33
ur5fot <wagvpered@gmail.com>
2015-11-05 23:20:20 +0200

    работает без ие9

---
+++ main.js
@@ -0,0 +1,30 @@
+/**
+ * Created by ur5fot on 05.11.15.
+ */
+(function () {
+    'use strict';
+    (function () {
+        'use strict';
+        document.addEventListener('DOMContentLoaded', function () {
+            document.addEventListener('click', attrClick);
+            function attrClick(event) {
+                var t = event.target;
+                if(t.hasAttributes('data-click')){
+                    t.classList.toggle(t.getAttribute('data-click'));
+                }else {
+                    return;
+                }
+            }
+            document.addEventListener('dblclick', attrDblclick);
+            function attrDblclick(event) {
+                var t = event.target;
+                if(t.hasAttributes('data-dblclick')){
+                    t.classList.toggle(t.getAttribute('data-dblclick'));
+                }else {
+                    return;
+                }
+            }
+        });
+    })();
+
+})();
