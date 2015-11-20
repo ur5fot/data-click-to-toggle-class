5ac7ce52ad5e2507221c9a8145d2053c9e37aad3
ur5fot <wagvpered@gmail.com>
2015-11-20 19:22:06 +0200

    работает как hover click dblclick отмена перехода по ссылке

--- dataAddClass.js
+++ dataAddClass.js
@@ -5,29 +5,38 @@ function dataAddClassBody() {
     document.addEventListener('dblclick', dblclickToggleClass);
 
     function clickToggleClass(e) {
-        var t = e.target;
-        if (t.hasAttribute('data-click')) {
-            document.body.classList.toggle(t.getAttribute('data-click'));
-        } else {
-            //return;
+        var tc = e.target;
+        var t = tc;
+        var te = e;
+
+        while (t && t !== document.documentElement) {
+            if (t.hasAttribute('data-click')) { //если нашли родителя с атребутом
+                document.body.classList.toggle(t.getAttribute('data-click'));
+                if (tc instanceof HTMLAnchorElement) {
+                    te.preventDefault();
+                }
+            }
+            t = t.parentNode; //перешли на родителя выше в остальных случаях
         }
     }
 
     function dblclickToggleClass(e) {
         var t = e.target;
-        if (t.hasAttribute('data-dblclick')) {
-            document.body.classList.toggle(t.getAttribute('data-dblclick'));
-            window.getSelection().removeAllRanges();
-        } else {
-            //return;
+        while (t && t !== document.documentElement) {
+            if (t.hasAttribute('data-dblclick')) { //если нашли родителя с атребутом
+                document.body.classList.toggle(t.getAttribute('data-dblclick'));
+                window.getSelection().removeAllRanges();
+            }
+            t = t.parentNode;
         }
     }
 
     document.addEventListener('mouseover', hoverAddClass);
     var hover = null;
+
     function hoverAddClass(e) {
         var t = e.target;
-        //цыкл выполняется если t есть и пока нестанет не дойдет до HTML и пока hover = null
+        //цикл выполняется если t есть и пока нестанет не дойдет до HTML и пока hover = null
         while (t && t !== document.documentElement && !hover) {
             if (t.hasAttribute('data-hover')) { //если нашли родителя с атребутом data-hover
                 document.body.classList.add(t.getAttribute('data-hover')); //доавили класс body
@@ -42,8 +51,8 @@ function dataAddClassBody() {
     document.addEventListener('mouseout', hoverRemoveClass);
     function hoverRemoveClass(e) {
         var t = e.target;
-        while (t && t  !== document.documentElement  && hover ) {
-            if (t === hover ) {
+        while (t && t !== document.documentElement && hover) {
+            if (t === hover) {
                 document.body.classList.remove(hover.getAttribute('data-hover'));
                 hover = null;
             }
